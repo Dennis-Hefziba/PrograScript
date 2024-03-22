1b962fc0791dd120b9a990d7e21899b65e01a3c2
Dennis-Hefziba <145822608+Dennis-Hefziba@users.noreply.github.com>
2024-03-22 18:22:54 +0000

    operaciones aritmeticas, suma, resta multi y division

--- intro.sh
+++ intro.sh
@@ -1,6 +1,13 @@
 
-read -p "Dime tu nombre: " nombre
-read -p "Cuantos anos tienes" edad
+read -p "Dime un numero " num1
+read -p "Dime otro numero " num2
 
-echo "Hola que tal como estas: $nombre: tu edad es: $edad"
+suma=$((num1+num2))
+resta=$((num1-num2))
+multiplicacion=$((num1*num2))
+division=$((num1/num2))
+echo "La suma de $num1 y $num2 es: $suma"
+echo "La resta de $num1 y $num2 es: $resta"
+echo "La division de $num1 y $num2 es: $division"
+echo "La multiplicacion de $num1 y $num2 es: $multiplicacion"
 
