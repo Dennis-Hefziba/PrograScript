f735825a5c5ddb7e9cf2974077c3d38ae4fe0947
Dennis-Hefziba <145822608+Dennis-Hefziba@users.noreply.github.com>
2024-04-07 15:25:12 +0000

    busqueda de info y remplazo con bash

--- analisis_datos.sh
+++ analisis_datos.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #contar el numero de palabras que contiene el archivo texto
 archivo=$1
+buscar=$2
+reemplazar=$3
 #si el archivo no existe
 if [ ! -e "$archivo" ]; then
     echo "Este archivo no existe"
     exit 1
 fi
-#si el archio existe
-wc -w $archivo
+#buscaruna palabra en un archivo y reemplazarla por otra
+sed -i "s/$buscar/$reemplazar/g" $archivo
