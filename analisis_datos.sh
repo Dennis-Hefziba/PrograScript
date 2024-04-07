750dd94db0886b71f8eecb082d24955552e7e4ab
Dennis-Hefziba <145822608+Dennis-Hefziba@users.noreply.github.com>
2024-04-07 15:10:31 +0000

    analisando noras promedio

--- analisis_datos.sh
+++ analisis_datos.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
-
+suma=0
+notaAlta=0
+notaMasBaja=10
+promedio=0
 while IFS=$'\t' read -r codigo nombre municipio departamento nota
 do
-    echo "Nombre: $nombre"
-    echo "Nota: $nota"
+    
+    suma=$((suma + nota))   
+    if [ $nota -gt $notaAlta ]; then
+        notaAlta=$nota
+        fi
+
+    if [ $nota -lt $notaMasBaja ]; then
+        notaMasBaja=$nota
+        fi
+
 done < <(tail -n +2 notas.csv)
+promedio=$((suma / 8))
+
+echo "la nota mas alta es: $notaAlta"
+echo "la nota mas baja es: $notaMasBaja"
+echo "el promedio es: $promedio"
