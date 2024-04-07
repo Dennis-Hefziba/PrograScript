ae8805f1385c2ce9cbdc2b5f3e7949a12e38e6b7
Dennis-Hefziba <145822608+Dennis-Hefziba@users.noreply.github.com>
2024-04-07 15:33:22 +0000

    busqueda avanzada por medio de patrones

--- analisis_datos.sh
+++ analisis_datos.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-#contar el numero de palabras que contiene el archivo texto
+
 archivo=$1
-buscar=$2
-reemplazar=$3
+EmailAbuscar=$2
+
 #si el archivo no existe
 if [ ! -e "$archivo" ]; then
     echo "Este archivo no existe"
     exit 1
 fi
-#buscaruna palabra en un archivo y reemplazarla por otra
-sed -i "s/$buscar/$reemplazar/g" $archivo
+
+#buscar un email en el archivo
+#grep $EmailAbuscar $archivo
+
+#buscar todos los correos 
+grep -oP '\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b' $archivo
