8d23c8302e3422397541758e4473387262065310
Dennis-Hefziba <145822608+Dennis-Hefziba@users.noreply.github.com>
2024-04-07 15:39:52 +0000

    Busqueda mediante patrones en bash

--- analisis_datos.sh
+++ analisis_datos.sh
@@ -14,3 +14,8 @@ fi
 
 #buscar todos los correos 
 grep -oP '\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b' $archivo
+
+#buscar dui
+#de los nums de 0 a 9 solo 8 digitos, de los nums de 0 a 9 1 digito (Patron de los duis)
+
+grep -oP '\b[0-9]{8}-[0-9]{1}\b' $archivo
